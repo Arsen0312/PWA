fc1ffbcfdbd93a4aa0dd5e3f63d19e037c6fe70f
Arsen0312 <greenchay16@gmail.com>
2025-08-06 02:15:32 +0600

    change height drawer

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 
 import 'react-spring-bottom-sheet/dist/style.css'
 import {TwoGisMap} from "@/components/TwoGisMap/TwoGisMap";
-import {BottomSheet} from "react-spring-bottom-sheet";
+import {BottomSheet, BottomSheetRef} from "react-spring-bottom-sheet";
 import DrawerContent from "@/components/DrawerContent/DrawerContent";
+import React, { useRef, useState} from "react";
 
 const Page = () => {
+    const [expandOnContentDrag] = useState(true)
+    const sheetRef = useRef<BottomSheetRef>(null)
 
     return (
         <main className="w-full h-full">
@@ -13,12 +16,17 @@ const Page = () => {
                 <TwoGisMap/>
             </div>
             <BottomSheet
-                open={true}
-                onDismiss={() => {}}
-                snapPoints={({ maxHeight }) => [maxHeight * 0.156, maxHeight * 0.56, maxHeight * 0.932]}
-                defaultSnap={({ snapPoints }) => snapPoints[3]}
+                open
+                skipInitialTransition
+                ref={sheetRef}
+                defaultSnap={({ maxHeight }) => maxHeight / 2}
+                snapPoints={({ maxHeight }) => [
+                    maxHeight - maxHeight / 10,
+                    maxHeight / 4,
+                    maxHeight * 0.6,
+                ]}
+                expandOnContentDrag={expandOnContentDrag}
                 blocking={false}
-                expandOnContentDrag
             >
                 <DrawerContent/>
             </BottomSheet>
@@ -27,3 +35,4 @@ const Page = () => {
 };
 
 export default Page;
+
