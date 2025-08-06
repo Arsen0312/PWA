5f764a3a073d76fef33e79cfe0a0487725df09d0
Arsen0312 <greenchay16@gmail.com>
2025-08-06 16:36:28 +0600

    change icon

---
+++ app/src/main/java/app/vercel/pwa_lac_pi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.pwa_lac_pi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
